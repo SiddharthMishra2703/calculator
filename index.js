f87c15cb5f62456595773b764e41fd4de2167f41
Siddharth Mishra <107067738+SiddharthMishra2703@users.noreply.github.com>
2022-09-28 09:40:45 +0530

    Intermediate files of this project

---
+++ index.js
@@ -0,0 +1,38 @@
+var str = "";
+var solve = "";
+$("button").click(function(){
+
+    //calculator logic
+    if($(this).html() == "x"){
+        str += $(this).html();
+        solve += "*";
+    }
+    else if($(this).html() == "÷"){
+        str += $(this).html();
+        solve += "/";
+    }
+    else if($(this).html() == "C"){
+        str = "";
+        solve = "";
+    }
+    else if($(this).html() == "D"){
+        str = str.slice(0,str.length - 1);
+        solve = str;
+    }
+    else if($(this).html() == "="){
+        str = eval(solve);
+        solve = str;
+    }
+    else{
+        str += $(this).html();
+        solve += $(this).html();
+    }
+
+    if(str.length > 7){
+        $("#input").addClass("small");
+    }else{
+        $("#input").removeClass("small");
+    }
+    $("#input").html(str);
+    console.log($(this).html());
+})
